338df6c6258e82a8935f9e6d77c92a6497bcd1ac
ShabbirAhamedSyed <153163554+ShabbirAhamedSyed@users.noreply.github.com>
2023-12-12 22:00:46 +0530

    added comments

--- spring_project-1/src/main/java/com/main/app/services/CustomerService.java
+++ spring_project-1/src/main/java/com/main/app/services/CustomerService.java
@@ -9,32 +9,37 @@ import org.springframework.stereotype.Service;
 import com.main.app.dao.CustomerRepo;
 import com.main.app.model.Customer;
 // In this class we have all methods to interact with DAO(Repository)
-// mark class as service it will have it's business logic 
+// the below annotation mark class as service it will have it's business logic 
 // it create spring bean managed by spring IOC(creating,initializing,destruction)  for dependencies injection
 @Service
 public class CustomerService {
+	// dependency injection of CustomerRepo automatically
 	@Autowired
 	private CustomerRepo repo;
-	
+	// this method will call the findAll method in repo and returns the List<Map<String, Object>> of all customers
 	public List<Map<String, Object>> getAllCus(){
 		List<Map<String, Object>> res = this.repo.findAll();
 		return res;
 		
 	}
+	//  this method will call the findById method in repo and returns the List<Map<String, Object>> of all customers
 	public  List<Map<String, Object>> getById(int id){
 		 List<Map<String, Object>> res = this.repo.findById(id);
 		 return res;
 	}
+	 // this method will call the insertNewCus method in repo and returns the integer
 	public int insertCus(Customer cus) {
 		// TODO Auto-generated method stub
 		int res = this.repo.insertNewCus(cus);
 		return res;
 	}
+	//  this method will call the update method in repo and returns the integer
 	public int updateCus(Customer cus) {
 		// TODO Auto-generated method stub
 		int res = this.repo.update(cus);
 		return res;
 	}
+	//  this method will call the delete method in repo and returns the integer
 	public int delById(int id) {
 		// TODO Auto-generated method stub
 		int res = this.repo.delete(id);
